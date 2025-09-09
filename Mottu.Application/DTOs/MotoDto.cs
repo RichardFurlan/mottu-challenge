15c9645612d7ba62c23a5ee5699ac879c8e61923
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2025-09-09 20:32:21 +0200

    feat: create DTOs

---
+++ Mottu.Application/DTOs/MotoDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Mottu.Application.DTOs;
+
+public record MotoDto(Guid Id, int Ano, string Modelo, string Placa, DateTime CreatedAt);
---
+++ Mottu.Domain/Entities/Moto2024.cs
@@ -0,0 +1,10 @@
+﻿namespace Mottu.Domain.Entities;
+
+public class Moto2024
+{
+    public Guid Id { get; set; }
+    public int Ano { get; set; }
+    public string Modelo { get; set; }
+    public string Placa { get; set; }
+    public DateTime CriadoEm { get; set; }
+}
