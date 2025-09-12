b2943a2082eea3360b16cee3bb0835ad21ffef85
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2025-09-13 00:05:12 +0200

    feat(api): implements controllers entregador and rental and change moto controller

---
+++ Mottu.Api/Controllers/EntregadoresController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mottu.Application.DTOs;
+using Mottu.Application.Services;
+
+namespace Mottu.Api.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class EntregadoresController : ControllerBase
+{
+    private readonly IEntregadorService _entregadorService;
+
+    public EntregadoresController(IEntregadorService entregadorService)
+    {
+        _entregadorService = entregadorService;
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create([FromBody] CreateEntregadorDto dto)
+    {
+        var result = await _entregadorService.CreateAsync(dto);
+        if (!result.IsSuccess) return BadRequest(result);
+        var location = $"/entregadores/{result.Data}";
+        return Created(location, new { id = result.Data });
+    }
+    
+    [HttpPost("{id:guid}/cnh")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> UploadCnh(Guid id,[FromForm] UploadCnhRequest request)
+    {
+        var result = await _entregadorService.UploadCnhAsync(id, request.File);
+        if (!result.IsSuccess) return BadRequest(result);
+        return Ok(result);
+    }
+}
---
+++ Mottu.Api/Controllers/RentalsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mottu.Application.DTOs;
+using Mottu.Application.Services;
+
+namespace Mottu.Api.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class RentalsController : ControllerBase
+{
+    private readonly IRentalService _rentalService;
+
+    public RentalsController(IRentalService rentalService)
+    {
+        _rentalService = rentalService;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateRentalDto dto)
+    {
+        var result = await _rentalService.CreateAsync(dto);
+        if (!result.IsSuccess) 
+            return BadRequest(result);
+        return CreatedAtAction(nameof(GetById), new { id = result.Data }, result);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var result = await _rentalService.GetByIdAsync(id);
+        if (!result.IsSuccess) 
+            return NotFound(result);
+        return Ok(result);
+    }
+
+    [HttpPost("{id:guid}/return")]
+    public async Task<IActionResult> Return(Guid id, [FromBody] ReturnRentalDto dto)
+    {
+        var result = await _rentalService.ReturnRentalAsync(id, dto);
+        if (!result.IsSuccess) 
+            return BadRequest(result);
+        return Ok(result); 
+    }
+}
